65ed8a2e8a1f443ce7cb90af6120fb2ecabe3261
shivanksharma4 <shivanksharma013@gmail.com>
2024-07-03 03:37:32 +0530

    secondary container with movieList

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,9 +5,12 @@ export const BACKGROUND =
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjcwMWY0NGI3YjdkYzVhYjNhZjRlZTFmNmRjODU2MCIsIm5iZiI6MTcxOTg2Nzc1Mi4wOTkyMzQsInN1YiI6IjY2ODMwYTc0ZmU2ODBlMzFmZTg1YWYxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N5AFi4FNX-qdS0LOE69B29V_-yHg0HW7Mdhh-lnt7lM'
-    }
-  };
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjcwMWY0NGI3YjdkYzVhYjNhZjRlZTFmNmRjODU2MCIsIm5iZiI6MTcxOTg2Nzc1Mi4wOTkyMzQsInN1YiI6IjY2ODMwYTc0ZmU2ODBlMzFmZTg1YWYxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N5AFi4FNX-qdS0LOE69B29V_-yHg0HW7Mdhh-lnt7lM",
+  },
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
