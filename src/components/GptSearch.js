9addefddb653a07ef6dabbf0515c8208251c1eed
shivanksharma4 <shivanksharma013@gmail.com>
2024-07-05 01:24:43 +0530

    Search bar & multi language support

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BACKGROUND } from '../utils/constants';
+import GptSearchBar from './GptSearchBar';
+import GptMovieSuggestions from './GptMovieSuggestions';
 
 const GptSearch = () => {
-  return (
-    <div>GptSearch</div>
-  )
+    return (
+        <div>
+          <div className="absolute -z-10">
+            <img src={BACKGROUND} alt="logo" />
+          </div>
+          <GptSearchBar />
+          <GptMovieSuggestions />
+        </div>
+      );
 }
 
 export default GptSearch
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ import { auth } from "../utils/firebase";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO } from "../utils/constants";
 import { toggleGptSearchView } from "../utils/gptSlice";
-
+import { SUPPORTED_LANGUAGES } from "../utils/constants";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -47,6 +48,10 @@ const Header = () => {
     dispatch(toggleGptSearchView());
   };
 
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
@@ -56,6 +61,18 @@ const Header = () => {
       />
       {user && (
         <div className="flex p-2">
+            {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
            <button
             className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
             onClick={handleGptSearchClick}
