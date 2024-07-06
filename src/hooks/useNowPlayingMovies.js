7f2c98a5f5832c61442707ab3be9885d4c7bbcee
shivanksharma4 <shivanksharma013@gmail.com>
2024-07-06 18:44:17 +0530

    Memoisation

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react"
 import { API_OPTIONS } from "../utils/constants"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { addNowPlayingMovies } from "../utils/movieSlice";
 
 const useNowPlayingMovies = ()=>{
   const dispatch = useDispatch();
+  const nowPlayingMovies = useSelector((store)=>store.movies.nowPlayingMovies)
   const getMoviesList = async()=>{
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS)
     const json = await data.json()
@@ -12,7 +13,7 @@ const useNowPlayingMovies = ()=>{
   }  
 
   useEffect(()=>{
-    getMoviesList()
+   !nowPlayingMovies && getMoviesList()
   },[])
 }
 
